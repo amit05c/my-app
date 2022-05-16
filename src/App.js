08ac48d851b0a135ae91b493ab81edc20aef623b
amit05c <101566257+amit05c@users.noreply.github.com>
2022-05-17 00:09:14 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Data from './components/Operating';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Data/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Amit
         </a>
       </header>
     </div>
---
+++ src/components/Operating.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Data=()=> {
+    return
+    <h1></h1>
+   
+   
+}
+
+export default Data
